7339d40c4f26dc649a78833d9140317c01eba772
admin <806802194@qq.com>
2018-03-30 11:42:01 +0800

    first

--- pages/carde/serch/index.js
+++ pages/carde/serch/index.js
@@ -1,4 +1,4 @@
-// pages/carde/index.js
+// pages/carde/serch/index.js
 Page({
 
   /**
@@ -14,11 +14,7 @@ Page({
   onLoad: function (options) {
   
   },
-  onFilterInput: function (){
-    wx.navigateTo({
-      url: 'pages/carde/serch/index',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
